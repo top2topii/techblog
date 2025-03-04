5da6291bf802694dcb054f07ca62f9dc0f666c51
Joseph Han <top2topii@gmail.com>
2025-03-05 00:18:05 +0900

    site:first commit

---
+++ assets/js/data/swconf.js
@@ -0,0 +1,30 @@
+const swconf = {
+  
+    cacheName: 'chirpy-1741101294',resources: [
+      '/techblog/assets/css/jekyll-theme-chirpy.css',
+      '/techblog/',
+      
+        '/techblog/categories/',
+      
+        '/techblog/tags/',
+      
+        '/techblog/archives/',
+      
+        '/techblog/about/',
+      
+
+      
+      
+    ],
+
+    interceptor: {paths: [
+        
+      ],urlPrefixes: [
+        
+      ]
+    },
+
+    purge: false
+  
+};
+
